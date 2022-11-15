0aff063d210cfb30df232b7aa1ea45e487b71ac6
baig78 <mirzanaimathbaig@gmail.com>
2022-11-15 09:55:30 +0530

    all api done

--- index.js
+++ index.js
@@ -201,8 +201,8 @@ app.put("/edit_user", (req, resp) => {
 app.put("/edit_rtr", (req, resp) => {
     const data=req.body;
     console.log(data)
-    let insertData = [data.name, data.email, data.user_name, data.phone, data.role, data.id];
-    con.query("UPDATE tbl_retailers SET name = ?, email = ?, user_name = ?, phone = ?, role = ? where id = ?",insertData, 
+    let insertData = [data.address, data.alt_phone, data.city, data.email, data.gst_no, data.name, data.phone, data.shop_name, data.id];
+    con.query("UPDATE tbl_retailers SET name = ?, alt_phone = ?, city = ?, email = ?, gst_no = ?, name = ?, phone = ?, shop_name = ? where id = ?",insertData, 
     (err, results) => {
         if (err) throw err;
         
@@ -229,6 +229,43 @@ app.put("/edit_bill", (req, resp) => {
     console.log(data)
     let insertData = [data.invoice_no, data.prd_batch_uid, data.rtr_id, data.qty, data.description, data.advance, data.balance, data.total, data.balance_due_date, data.insert_date, data.updated_date, data.gen_otp, data.otp_status, data.insert_user_id, data.update_user_id, data.id];
     con.query("UPDATE tbl_bill SET invoice_no=?, prd_batch_uid=?, rtr_id=?, qty=?, description=?, advance=?, balance=?, total=?, balance_due_date=?, insert_date=?, updated_date=?, gen_otp=?, otp_status=?, insert_user_id=?, update_user_id=?  where id=?",insertData, 
+    (err, results) => {
+        if (err) throw err;
+        
+            resp.send(results)
+        
+    })
+})
+app.put("/edit_pbatch", (req, resp) => {
+    const data=req.body;
+    console.log(data)
+    let insertData = [data.uid, data.mfr_id, data.batch, data.model_uid, data.color, data.size, data.mfr_price, data.per_comm, data.mfr_gst, data.rtr_gst, data.available, data.sold,  data.id];
+    con.query("UPDATE tbl_products_batch SET uid = ?, mfr_id = ?, batch = ?, model_uid = ?, color = ?, size = ?, mfr_price = ?, per_comm = ?, mfr_gst = ?, rtr_gst = ?, available = ?, sold = ? where id = ?",insertData, 
+    (err, results) => {
+        if (err) throw err;
+        
+            resp.send(results)
+        
+    })
+})
+
+app.put("/edit_pmodels", (req, resp) => {
+    const data=req.body;
+    console.log(data)
+    let insertData = [data.uid, data.name,  data.id];
+    con.query("UPDATE tbl_product_models SET uid = ?, name = ?  where id = ?",insertData, 
+    (err, results) => {
+        if (err) throw err;
+        
+            resp.send(results)
+        
+    })
+})
+app.put("/edit_payment", (req, resp) => {
+    const data=req.body;
+    console.log(data)
+    let insertData = [data.invoice_no, data.payment_type, data.insert_date, data.updated_date, data.insert_user_id, data.update_user_id,  data.id];
+    con.query("UPDATE tbl_payments SET invoice_no = ?, payment_type = ?, insert_date = ?, updated_date = ?, insert_user_id = ?, update_user_id = ?  where id = ?",insertData, 
     (err, results) => {
         if (err) throw err;
         
@@ -273,6 +310,30 @@ app.get("/get_mfr/:id", async(req, resp) => {
         console.log("1 record deleted");
     });
 })
+app.get("/get_rtr/:id", async(req, resp) => {
+
+    con.query('SELECT * FROM tbl_retailers WHERE id ='+req.params.id,  (err, result)=> {
+        if (err) throw err;
+        resp.send(result)
+        console.log("1 record deleted");
+    });
+})
+app.get("/get_product_models/:id", async(req, resp) => {
+
+    con.query('SELECT * FROM tbl_product_models WHERE id ='+req.params.id,  (err, result)=> {
+        if (err) throw err;
+        resp.send(result)
+        console.log("1 record deleted");
+    });
+})
+app.get("/get_product_batch/:id", async(req, resp) => {
+
+    con.query('SELECT * FROM tbl_products_batch WHERE id ='+req.params.id,  (err, result)=> {
+        if (err) throw err;
+        resp.send(result)
+        console.log("1 record deleted");
+    });
+})
 
 // ----------------get API--------------------
 
@@ -300,6 +361,42 @@ app.delete("/delete_mfr/:id", async(req, resp) => {
 app.delete("/delete_bill/:id", async(req, resp) => {
 
     con.query('DELETE FROM tbl_bill WHERE id ='+req.params.id,  (err, result)=> {
+        if (err) throw err;
+        resp.send({'message':'success'})
+        console.log("1 record deleted");
+    });
+})
+
+app.delete("/delete_rtr/:id", async(req, resp) => {
+
+    con.query('DELETE FROM tbl_retailers WHERE id ='+req.params.id,  (err, result)=> {
+        if (err) throw err;
+        resp.send({'message':'success'})
+        console.log("1 record deleted");
+    });
+})
+
+app.delete("/delete_product_model/:id", async(req, resp) => {
+
+    con.query('DELETE FROM tbl_product_models WHERE id ='+req.params.id,  (err, result)=> {
+        if (err) throw err;
+        resp.send({'message':'success'})
+        console.log("1 record deleted");
+    });
+})
+
+app.delete("/delete_product_batch/:id", async(req, resp) => {
+
+    con.query('DELETE FROM tbl_products_batch WHERE id ='+req.params.id,  (err, result)=> {
+        if (err) throw err;
+        resp.send({'message':'success'})
+        console.log("1 record deleted");
+    });
+})
+
+app.delete("/delete_payment/:id", async(req, resp) => {
+
+    con.query('DELETE FROM tbl_payments WHERE id ='+req.params.id,  (err, result)=> {
         if (err) throw err;
         resp.send({'message':'success'})
         console.log("1 record deleted");
